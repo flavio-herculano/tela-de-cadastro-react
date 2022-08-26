c09aee655c6cb0e12f77fd7f1a5661a4a96d926b
Flávio Herculano <flavio.hercullano@gmail.com>
2022-08-26 17:07:38 -0300

    adicionado cadastro e logo e estilo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const { register, handleSubmit, setValue, setFocus } = useForm();
+
+  const onSubmit = (e) => {
+    console.log(e);
+  };
+
+  const checkCEP = (e) => {
+    const cep = e.target.value.replace(/\D/g, "");
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        register();
+        setValue("address", data.logradouro);
+        setValue("neighborhood", data.bairro);
+        setValue("city", data.localidade);
+        setValue("uf", data.uf);
+        setFocus("addressNumber");
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <header className="container">
+        <a className="logo" href="##">
+          SIL SOLUTIONS
         </a>
+        <p className="subtitle">Sistema Inteligente para Laboratórios</p>
       </header>
-    </div>
+
+      <section className="section">
+        <h1 className="title">CADASTRO</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>
+            Nome
+            <input type="text" {...register("name")} />
+          </label>
+          <label>
+            Sobrenome
+            <input type="text" {...register("lastname")} />
+          </label>
+          <label>
+            Data de Nascimento
+            <input type="text" {...register("birthday")} />
+          </label>
+          <label>
+            CPF
+            <input type="text" {...register("cpf")} />
+          </label>
+          <label>
+            RG
+            <input type="text" {...register("rg")} />
+          </label>
+          <label>
+            CEP
+            <input type="text" {...register("cep")} onBlur={checkCEP} />
+          </label>
+          <label>
+            Rua
+            <input type="text" {...register("address")} />
+          </label>
+          <label>
+            Número
+            <input type="text" {...register("addressNumber")} />
+          </label>
+          <label>
+            Bairro
+            <input type="text" {...register("neighborhood")} />
+          </label>
+          <label>
+            Cidade
+            <input type="text" {...register("city")} />
+          </label>
+          <label>
+            Estado
+            <input type="text" {...register("uf")} />
+          </label>
+          <button type="submit">Enviar</button>
+        </form>
+      </section>
+    </>
   );
 }
 
