6ee8f442d910d86d2029e57dd13ee914eea90f3d
Flávio Herculano <flavio.hercullano@gmail.com>
2022-08-27 05:34:56 -0300

    adicionado estilo e data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import "./App.css";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+export function BasicDatePicker() {
+  const [value, setValue] = React.useState(null);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label="Data de Nascimento"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+          console.log(newValue.$d);
+        }}
+        renderInput={(params) => <TextField variant="filled" {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
 
 function App() {
-  const { register, handleSubmit, setValue, setFocus } = useForm();
+  const { register, setValue, handleSubmit, setFocus } = useForm();
 
   const onSubmit = (e) => {
     console.log(e);
@@ -12,7 +33,7 @@ function App() {
 
   const checkCEP = (e) => {
     const cep = e.target.value.replace(/\D/g, "");
-    console.log(cep);
+
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
       .then((res) => res.json())
       .then((data) => {
@@ -40,52 +61,79 @@ function App() {
 
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid">
-            <label>
-              Nome
-              <input type="text" {...register("name")} />
-            </label>
-            <label>
-              Sobrenome
-              <input type="text" {...register("lastname")} />
-            </label>
-            <label>
-              Data de Nascimento
-              <input type="date" {...register("birthday")} />
-            </label>
-            <label>
-              CPF
-              <input type="text" {...register("cpf")} />
-            </label>
-            <label>
-              RG
-              <input type="text" {...register("rg")} />
-            </label>
+            <TextField
+              id="filled-basic"
+              label="Nome"
+              variant="filled"
+              {...register("name")}
+            />
+            <TextField
+              id="filled-basic"
+              label="Sobrenome"
+              variant="filled"
+              {...register("lastname")}
+            />
+            <BasicDatePicker></BasicDatePicker>
+
+            <TextField
+              id="filled-basic"
+              label="CPF"
+              variant="filled"
+              {...register("cpf")}
+            />
+            <TextField
+              id="filled-basic"
+              label="RG"
+              variant="filled"
+              {...register("RG")}
+            />
           </div>
           <div className="grid">
-            <label>
-              CEP
-              <input type="text" {...register("cep")} onBlur={checkCEP} />
-            </label>
-            <label>
-              Rua
-              <input type="text" {...register("address")} />
-            </label>
-            <label>
-              Número
-              <input type="text" {...register("addressNumber")} />
-            </label>
-            <label>
-              Bairro
-              <input type="text" {...register("neighborhood")} />
-            </label>
-            <label>
-              Cidade
-              <input type="text" {...register("city")} />
-            </label>
-            <label>
-              Estado
-              <input type="text" {...register("uf")} />
-            </label>
+            <TextField
+              id="filled-basic"
+              label="CEP"
+              variant="filled"
+              {...register("cep")}
+              onBlur={checkCEP}
+            />
+            <TextField
+              id="filled-basic"
+              label="Rua"
+              focused="true"
+              color="grey"
+              variant="filled"
+              {...register("address")}
+            />
+            <TextField
+              id="filled-basic"
+              label="Número"
+              variant="filled"
+              {...register("addressNumber")}
+            />
+            <TextField
+              id="filled-basic"
+              label="Bairro"
+              focused="true"
+              color="grey"
+              variant="filled"
+              {...register("neighborhood")}
+            />
+            <TextField
+              id="filled-basic"
+              label="Cidade"
+              focused="true"
+              color="grey"
+              variant="filled"
+              {...register("city")}
+            />
+            <TextField
+              id="filled-basic"
+              label="Estado"
+              variant="filled"
+              focused="true"
+              color="grey"
+              {...register("uf")}
+            />
           </div>
           <Button variant="contained" type="submit">
             Enviar
